c556f86f44b77344159e5eaaf4aaddfe43cdb463
hibathakur559-boop <hibathakur559@gmail.com>
2026-09-19 03:29:47 +0500

    Add files via upload

---
+++ project/src/App.tsx
@@ -0,0 +1,13 @@
+import LandingPage from '@/components/LandingPage';
+import AvatarWidget from '@/components/AvatarWidget';
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+      <AvatarWidget />
+    </>
+  );
+}
+
+export default App;
